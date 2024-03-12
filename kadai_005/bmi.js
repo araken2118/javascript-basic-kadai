b3d44efe1fba00e8f8c2d060b3909841167473e2
araken2118 <72079619+araken2118@users.noreply.github.com>
2024-03-12 09:09:17 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,9 @@
+//変数の宣言・値の代入
+let userWeight = 68;
+let userHight = 1.7;
+let userBMI = userWeight/(userHight*userHight);
+
+//コンソールへの出力
+console.log(userWeight);
+console.log(userHight);
+console.log(userBMI);
